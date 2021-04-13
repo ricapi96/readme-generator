9923f840f6d8dc09165f09d3f5eb5d8a0a2aa1f1
Erica Roquemore <enroquemore@gmail.com>
2021-04-13 00:27:22 -0400

    updated questions

--- Develop/index.js
+++ Develop/index.js
@@ -9,6 +9,31 @@ const questions = [
     name: 'github',
     message: 'What is your Github username?'
     },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What kind of license should your project have?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?'
+    },
 ];
 
 // TODO: Create a function to write README file
