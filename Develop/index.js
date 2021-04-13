7c0ae7b20aebdd8d7234ad002f68f8d63b304322
Erica Roquemore <enroquemore@gmail.com>
2021-04-13 00:04:22 -0400

    updated index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,21 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    type: 'input',
+    name: 'github',
+    message: 'What is your Github username?'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then.
+}
 
 // Function call to initialize app
 init();
